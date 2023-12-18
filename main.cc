ed2f8e36a5ef60f4df71a61f398f95870791b9b5
Allocated Artist <Senorsiete2006@gmail.com>
2023-12-17 20:28:30 -0800

    shorten main

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include "src/Model.h"
 #include "src/FlyCamera.h"
 #include "src/PhysicsWorld.h"
+#include "src/PlayerMovement.h"
 
 
 #include <iostream>
@@ -42,7 +43,6 @@ bool CustomContactAdd(
 
 
 int main(void) {
-  
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(1600, 1480, "Platformer");
 
@@ -74,10 +74,7 @@ int main(void) {
   std::unique_ptr<btCollisionShape> box_shape = world.CreateBoxShape(
     { 0.5, 0.5, 0.5 }
   );
-
-  std::unique_ptr<btConvexShape> crouched_capsule = 
-    std::make_unique<btCapsuleShape>(0.2, 0.25);
-
+ 
   RigidBody floor = world.CreateRigidBody(
     Vector3Zero(), 
     floor_shape,
@@ -91,14 +88,6 @@ int main(void) {
     QuaternionIdentity(),
     0.0
   );
-
-  /*
-
-  floor.rigid_body_->setCollisionFlags(
-    floor.rigid_body_->getCollisionFlags() |
-    btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK
-  );
-  */
 
   std::vector<RigidBody> cubes;
 
@@ -137,140 +126,18 @@ int main(void) {
   ContactInfo info;
   player.ghost_object_->setUserPointer(&info);
 
+  PlayerMovement player_movement;
+
   SetTargetFPS(120);
-
-  Vector3 walk = Vector3Zero();  
-
-  float walk_speed = 2.0;
-  float run_speed = 4.0;
-  float crouch_speed = 0.3;
-  float current_speed = walk_speed;
-  
-  float stand_jump_height = 5.0;
-  float crouched_jump_height = 2.5;
-  float current_jump_height = stand_jump_height;
-
-  bool sliding = false;
-
-  float ground_friction = 0.4 / 10.0;
-  float slide_friction = 0.35 / 10.0;
-  float air_friction = 0.08 / 10.0;
 
   while (!WindowShouldClose()) { 
 
     world.Update(1.0 / 60.0);
 
     camera.LookAround();
-
-    Vector3 forward = camera.GetCamera().GetForward();
-    forward.y = 0.0;
-    Vector3 right = camera.GetCamera().GetRight();
-
-    if (IsKeyPressed(KEY_SPACE) && player.controller_->onGround()) {
-      if (sliding && Vector3Length(walk) > 0.01) {
-        
-        Vector3 forward_force = Vector3Scale(forward, 5.0);
-        player.controller_->applyImpulse(
-          btVector3(forward_force.x, stand_jump_height * 1.2, forward_force.z)
-        );
-      } else {
-        player.controller_->jump(btVector3(0.0, current_jump_height, 0.0)); 
-      }
-    }
-  
-    Vector3 move_dir = Vector3Zero();
-
-    if (IsKeyDown(KEY_W)) {
-      move_dir = Vector3Add(move_dir, forward);
-    }
-    if (IsKeyDown(KEY_S)) {
-      move_dir = Vector3Subtract(move_dir, forward);
-    }
-    if (IsKeyDown(KEY_A)) {
-      move_dir = Vector3Subtract(move_dir, right);
-    }
-    if (IsKeyDown(KEY_D)) {
-      move_dir = Vector3Add(move_dir, right);
-    } 
-
-    if (IsKeyDown(KEY_LEFT_SHIFT) && player.controller_->onGround()) { 
-      current_speed = Lerp(current_speed, run_speed, 0.2 / 10.0); 
-    } else {
-      if (player.controller_->onGround()) { 
-        current_speed = Lerp(current_speed, walk_speed, 0.2 / 10.0);
-      }
-    }
-
-    if (IsKeyDown(KEY_LEFT_CONTROL) && player.controller_->onGround()) {
-      current_jump_height = crouched_jump_height;
-      current_speed = Lerp(current_speed, crouch_speed, 0.5 / 10.0);
-      player.ghost_object_->setCollisionShape(crouched_capsule.get());
-
-      if (!Vector3Equals(move_dir, Vector3Zero())) {
-        btVector3 bt_forward = btVector3(move_dir.x, 0.0, move_dir.z);
-        float mag = Vector3Length(walk);
-
-        if (mag > 0.018f && !sliding) {
-          player.controller_->applyImpulse(bt_forward * 3.f);
-          sliding = true;
-        }
-      }
-    } else {
-      current_jump_height = stand_jump_height;
-      player.ghost_object_->setCollisionShape(player.convex_.get());
-    }
-
-    if (IsKeyUp(KEY_LEFT_CONTROL) && Vector3Length(walk) < 0.002) {
-      sliding = false;
-    }
-
- 
-    if (!Vector3Equals(move_dir, Vector3Zero()) && !sliding) {
-      move_dir = Vector3Normalize(move_dir);
-      walk = Vector3Scale(move_dir, current_speed * GetFrameTime());
-    } else {
-      if (player.controller_->onGround()) {
-        if (!sliding) {
-          walk = Vector3Lerp(walk, Vector3Zero(), ground_friction);
-        } else {
-          walk = Vector3Lerp(walk, Vector3Zero(), slide_friction);
-        }
-      } else {
-        walk = Vector3Lerp(walk, Vector3Zero(), air_friction);
-      }
-    } 
-
-    if (Vector3Length(walk) > 0.018f) {
-      float fov = camera.GetCamera().GetFOV();
-      fov = Lerp(fov, 100.f, 0.8f / 10.0);
-      camera.GetCamera().SetFOV(fov);
-    } else if (Vector3Length(walk) < 0.014) {
-      float fov = camera.GetCamera().GetFOV();
-      fov = Lerp(fov, 90.f, 0.8f / 10.0);
-      camera.GetCamera().SetFOV(fov);
-    }
-
-
-    player.controller_->setFallSpeed(8.f);
-
-    player.controller_->setWalkDirection(
-      btVector3(
-        walk.x,
-        0.0f, 
-        walk.z
-      )
-    ); 
-
-    btCapsuleShape* capsule = 
-      (btCapsuleShape*)player.ghost_object_->getCollisionShape();
-
-    camera.GetCamera().SetPosition(
-      Vector3Add(
-        conv::PosFromController(player),
-        { 0.f, capsule->getHalfHeight(), 0.f }
-      )
-    );
-     
+    
+    player_movement.Update(player, camera);
+    
     BeginDrawing(); 
     ClearBackground(BLACK);
 
@@ -317,8 +184,6 @@ int main(void) {
         return false;
       }
     );
-
-  
 
     if (cube_to_erase != cubes.cend()) {
       RigidBody& body = *cube_to_erase;   
