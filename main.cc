43d43a0d6d794782981a9afcd1b0ae9bd15dec53
Allocated Artist <Senorsiete2006@gmail.com>
2024-01-10 21:58:05 -0800

    saving/loading/level editing

--- main.cc
+++ main.cc
@@ -1,4 +1,4 @@
-#include <algorithm>
+#include <fstream>
 #include <raylib.h>
 
 #include "src/Model.h"
@@ -8,6 +8,8 @@
 #include "src/LevelEditor.h"
 
 #include <iostream>
+
+#include <json.hpp>
 
 int main(void) {
   SetConfigFlags(FLAG_MSAA_4X_HINT);
@@ -22,24 +24,64 @@ int main(void) {
   level_editor.UpdateThumbnails();
 
   std::vector<LevelMesh> meshes;
+  SetExitKey(KEY_NULL);
 
+  bool saved = false;
+  float save_timer = 0.f;
+  float save_delay = 1.0f;
+    
   while (!WindowShouldClose()) { 
 
-    level_editor.UpdateCamera(camera);
-           
+    level_editor.UpdateCamera(camera); 
+
+    level_editor.Save(meshes);
+
+    if (
+      IsKeyDown(KEY_LEFT_CONTROL) && 
+      IsKeyPressed(KEY_S) && 
+      saved == false
+    ) {
+      saved = true; 
+    }
+
+    level_editor.Load(meshes);
+     
     BeginDrawing(); 
     ClearBackground(BLACK);
  
     BeginMode3D(camera.GetCamera().GetCamera()); 
+
+    level_editor.PlacePlayer(camera);
     
-    level_editor.PlaceObjects(meshes, camera);
     DrawGrid(20, 1.0);
+
+    if (!level_editor.IsPlayerSetMode()) {
+      level_editor.PlaceObjects(meshes, camera);
+
+      for (LevelMesh& mesh : meshes) {
+        level_editor.SelectObject(mesh, camera);
+      }
+    }
 
     for (const LevelMesh& mesh : meshes) {
       level_editor.DrawAsset(mesh);
     }
  
     EndMode3D();
+
+    if (saved) {
+      save_timer += GetFrameTime();
+      if (save_timer >= save_delay) {
+        save_timer = 0.f;
+        saved = false;
+      }
+
+      const char* filename = level_editor.GetCurrentFileSaveName().c_str();
+      DrawText(
+        TextFormat("Saved to: %s", filename),
+        500, 100, 24, RED
+      );
+    }
 
     level_editor.DrawThumbnails();
 
